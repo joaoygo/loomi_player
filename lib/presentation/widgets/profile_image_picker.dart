97ecd6c81e9219a5fbe62631f1f30db1847d7d3c
joaoygo <joaoygo.freitas25@gmail.com>
2025-01-17 20:05:22 -0300

    [chore] create new items in profile image picker widget

--- lib/presentation/widgets/profile_image_picker.dart
+++ lib/presentation/widgets/profile_image_picker.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:loomi_player/core/constants/app_colors.dart';
+import 'package:loomi_player/core/constants/assets_constants.dart';
 
 class ProfileImagePicker extends StatefulWidget {
   final Function(File) onImagePicked;
@@ -47,38 +50,146 @@ class _ProfileImagePickerState extends State<ProfileImagePicker> {
       onTap: () {
         showModalBottomSheet(
           context: context,
+          backgroundColor: Color.fromRGBO(19, 20, 24, 1),
           builder: (_) => Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ListTile(
-                  leading: Icon(Icons.camera_alt),
-                  title: Text("Tirar foto"),
-                  onTap: () {
-                    _pickImage(ImageSource.camera);
-                    Navigator.of(context).pop();
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.photo),
-                  title: Text("Escolher da galeria"),
-                  onTap: () {
-                    _pickImage(ImageSource.gallery);
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
+            child: SizedBox(
+              height: 200,
+              child: Column(
+                children: [
+                  Text(
+                    'Choose Image',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'Montserrat',
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          _pickImage(ImageSource.camera);
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                          width: 135,
+                          height: 140,
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(188, 76, 241, 0.2),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: Color.fromRGBO(170, 115, 240, 1),
+                            ),
+                            image: _image != null
+                                ? DecorationImage(
+                                    image: FileImage(_image!),
+                                    fit: BoxFit.cover,
+                                  )
+                                : null,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset(
+                                AssetsConstants.cameraIcon,
+                                width: 24,
+                                height: 24,
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                'Take a photo',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: AppColors.primaryColorTextOpacity,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      GestureDetector(
+                        onTap: () {
+                          _pickImage(ImageSource.gallery);
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                          width: 135,
+                          height: 140,
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(187, 187, 187, 0.1),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: Color.fromRGBO(215, 215, 215, 0.6),
+                            ),
+                            image: _image != null
+                                ? DecorationImage(
+                                    image: FileImage(_image!),
+                                    fit: BoxFit.cover,
+                                  )
+                                : null,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.photo_outlined,
+                                size: 24,
+                                color: Colors.white,
+                              ),
+                              SizedBox(height: 8),
+                              SizedBox(
+                                width: 95,
+                                child: Text(
+                                  'Choose from gallery',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: AppColors.primaryColorTextOpacity,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         );
       },
-      child: CircleAvatar(
-        radius: 50,
-        backgroundColor: Colors.grey[200],
-        backgroundImage: _image != null ? FileImage(_image!) : null,
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+          color: Color.fromRGBO(188, 76, 241, 0.2),
+          borderRadius: BorderRadius.circular(20),
+          image: _image != null
+              ? DecorationImage(
+                  image: FileImage(_image!),
+                  fit: BoxFit.cover,
+                )
+              : null,
+        ),
         child: _image == null
-            ? Icon(Icons.camera_alt, size: 50, color: Colors.grey[700])
+            ? Center(
+                child: SvgPicture.asset(
+                  AssetsConstants.cameraIcon,
+                  width: 24,
+                  height: 24,
+                ),
+              )
             : null,
       ),
     );
